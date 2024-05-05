e6776fd2b61767c9b52d71a2c62b003c8041099d
Prashant <khadka.pst07@gmail.com>
2024-05-05 12:28:30 +0545

    login page added

--- script.js
+++ script.js
@@ -1,98 +1,26 @@
-let form = document.getElementById("form");
+/*===== LOGIN SHOW and HIDDEN =====*/
+const signUp = document.getElementById('sign-up'),
+    signIn = document.getElementById('sign-in'),
+    loginIn = document.getElementById('login-in'),
+    loginUp = document.getElementById('login-up')
 
-function validateName(name){
-    return(
-        name.length >=4 && name.length<=20
-    )
-}
 
-function validatePhone(phone){
-    return(
-        phone.length >=12 && phone.length<=9
-    )
-}
+signUp.addEventListener('click', ()=>{
+    // Remove classes first if they exist
+    loginIn.classList.remove('block')
+    loginUp.classList.remove('none')
 
-let emailErrorMessage = ["Email must contain @", "Email must contain '.'"]
-function validateEmail(email){
-    let split = email.split("@")
-    if(split.length == 1){
-        return 0
-    }
-
-    let checkDot = split[split.length-1]
-    let checkDoSplit = checkDot.split(".")
-    if(checkDoSplit.length ==1){
-        return 1
-    }
-    return -1;
-
-}
-
-function validateCountry(){
-    if(country.selectedIndex == 0){
-        return -1
-    }
-    return true;
-}
-
-function formSubmit(){
-    let name = document.getElementById("name");
-    if(!validateName(name.value)){
-        showError("Name length must be in range 4-20")
-        return
-    }
-     let email = document.getElementById("email")
-     let checkEmail = validateEmail(email.value)
-     if(checkEmail != -1){
-        showError(emailErrorMessage[checkEmail])
-        return
-     }
-
-     let countries = document.getElementById("country")
-     if(!validateCountry(countries)){
-        showError("Please select a country")
-        return
-     }
-     let country = countries.options[countries.selectedIndex].value
-
-    let phone = document.getElementById("phoneNumber")
-    if(!validatePhone(phone.value)){
-        showError("Phone Number Invalid")
-        return
-    }
-    clearError();
-}
-
-let error = document.getElementById("error");
-function showError(message){
-    
-    error.style.display = "block"
-    error.innerHTML = message
-}
-
-function clearError(){
-error.style.display = "none"
-}
-
-form.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    formSubmit();
+    // Add classes
+    loginIn.classList.toggle('none')
+    loginUp.classList.toggle('block')
 })
-var img = document.getElementById('slider-wrapper');
 
-var slides=['slider1.png','slider2.png', 'slider3.png'];
+signIn.addEventListener('click', ()=>{
+    // Remove classes first if they exist
+    loginIn.classList.remove('none')
+    loginUp.classList.remove('block')
 
-var Start=0;
-
-function slider(){
-    if(Start<slides.length){
-        Start=Start+1;
-    }
-    else{
-        Start=1;
-    }
-    console.log(img);
-    img.innerHTML = "<img src="+slides[Start-1]+">";
-   
-}
-setInterval(slider,2000);
+    // Add classes
+    loginIn.classList.toggle('block')
+    loginUp.classList.toggle('none')
+})
